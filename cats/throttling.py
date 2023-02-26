474408a082adf10976b58d27661997e892e69073
Роман Александров <rsaleksandrov@yandex.ru>
2023-02-26 13:23:31 +0300

    Добавили докстринг

--- cats/throttling.py
+++ cats/throttling.py
@@ -4,7 +4,7 @@ from rest_framework import throttling
 
 
 class WorkingHoursRateThrottle(throttling.BaseThrottle):
-
+    """Ограничение запросов в период с 12 до 13 часов."""
     def allow_request(self, request, view):
         now = datetime.datetime.now().hour
         if now >= 12 and now <= 13:
